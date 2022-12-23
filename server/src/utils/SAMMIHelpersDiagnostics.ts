de75f4d7964948b2438ea1684cc2f5115aa53d7f
SebasF <33849340+SebasF1349@users.noreply.github.com>
2022-12-23 10:52:15 -0300

    improve underline and alerts

--- server/src/utils/SAMMIHelpersDiagnostics.ts
+++ server/src/utils/SAMMIHelpersDiagnostics.ts
@@ -4,7 +4,7 @@ import { DiagnosticSeverity } from "vscode-languageserver/node";
 
 type SAMMIHelper = {
 	name: string;
-	args: number[];
+	args: [number, number]; // [min, max]
 };
 
 const SAMMIHelpers: SAMMIHelper[] = [
@@ -30,7 +30,7 @@ const SAMMIHelpers: SAMMIHelper[] = [
 	},
 	{
 		name: "extCommand",
-		args: [2, 3, 4, 5],
+		args: [2, 5],
 	},
 	{
 		name: "triggerExt",
@@ -38,75 +38,75 @@ const SAMMIHelpers: SAMMIHelper[] = [
 	},
 	{
 		name: "triggerButton",
-		args: [1],
+		args: [1, 1],
 	},
 	{
 		name: "modifyButton",
-		args: [0, 5],
+		args: [1, 5],
 	},
 	{
 		name: "popUp",
-		args: [1],
+		args: [1, 1],
 	},
 	{
 		name: "alert",
-		args: [1],
+		args: [1, 1],
 	},
 	{
 		name: "notification",
-		args: [1],
+		args: [1, 1],
 	},
 	{
 		name: "getDeckList",
-		args: [0],
+		args: [0, 0],
 	},
 	{
 		name: "getDeck",
-		args: [1],
+		args: [1, 1],
 	},
 	{
 		name: "getDeckStatus",
-		args: [1],
+		args: [1, 1],
 	},
 	{
 		name: "changeDeckStatus",
-		args: [2],
+		args: [2, 2],
 	},
 	{
 		name: "getImage",
-		args: [1],
+		args: [1, 1],
 	},
 	{
 		name: "getSum",
-		args: [1],
+		args: [1, 1],
 	},
 	{
 		name: "stayInformed",
-		args: [1],
+		args: [1, 1],
 	},
 	{
 		name: "getActiveButtons",
-		args: [0],
+		args: [0, 0],
 	},
 	{
 		name: "getModifiedButtons",
-		args: [0],
+		args: [0, 0],
 	},
 	{
 		name: "getTwitchList",
-		args: [0],
+		args: [0, 0],
 	},
 	{
 		name: "trigger",
-		args: [2],
+		args: [2, 2],
 	},
 	{
 		name: "close",
-		args: [0],
+		args: [0, 0],
 	},
 	{
 		name: "generateMessage",
-		args: [0],
+		args: [0, 0],
 	},
 ];
 
@@ -116,17 +116,31 @@ export function sammiDiagnostics(document: TextDocument) {
 	for (let i = 0; i < SAMMIHelpers.length; i++) {
 		let helper = fullText.indexOf("SAMMI." + SAMMIHelpers[i].name + "(");
 		while (helper !== -1) {
+			const min = SAMMIHelpers[i].args[0];
+			const max = SAMMIHelpers[i].args[1];
 			const argsStart = fullText.indexOf("(", helper) + 1;
 			const possibleArgText = fullText.slice(argsStart);
-			const args = countCommas(possibleArgText);
+			const args = countCommas(possibleArgText, max);
 			let argsNumber = args.commasCount + 1;
-			const argsLength = args.argsLength;
-			if (argsLength === 0) argsNumber = 0;
-			if (!SAMMIHelpers[i].args.includes(argsNumber)) {
+			if (args.argsLength === 0) argsNumber = 0;
+			if (argsNumber < min || argsNumber > max) {
+				let diagnosticStart: number;
+				const diagnosticEnd = argsStart + args.endArgs;
+				let message: string;
+				if (argsNumber < min) {
+					diagnosticStart = argsStart - SAMMIHelpers[i].name.length - 1;
+				} else {
+					diagnosticStart = argsStart + args.excessArgs;
+				}
+				if (min === max) {
+					message = `Expected ${min} arguments, but got ${argsNumber}.`;
+				} else {
+					message = `Expected ${min}-${max} arguments, but got ${argsNumber}.`;
+				}
 				diagnostics.push(
 					Diagnostic.create(
-						Range.create(document.positionAt(argsStart), document.positionAt(argsStart + args.endArgs)),
-						`Incorrect number of arguments`,
+						Range.create(document.positionAt(diagnosticStart), document.positionAt(diagnosticEnd)),
+						message,
 						DiagnosticSeverity.Error
 					)
 				);
@@ -137,7 +151,7 @@ export function sammiDiagnostics(document: TextDocument) {
 	return diagnostics;
 }
 
-function countCommas(text: string) {
+function countCommas(text: string, max: number) {
 	const wrappers = new Map([
 		["{", "}"],
 		["[", "]"],
@@ -147,6 +161,7 @@ function countCommas(text: string) {
 	const layers: string[] = [];
 	let commasCount = 0;
 	let endArgs = text.length - 1;
+	let excessArgs = -1;
 	for (let i = 0; i < text.length; i++) {
 		const char = text[i];
 		const layersDepth = layers.length;
@@ -163,6 +178,9 @@ function countCommas(text: string) {
 		} else {
 			if (char === ",") {
 				commasCount++;
+				if (commasCount === max) {
+					excessArgs = i;
+				}
 			} else if (char === ")") {
 				endArgs = i;
 				break;
@@ -173,5 +191,5 @@ function countCommas(text: string) {
 		}
 	}
 	const argsLength = text.slice(0, endArgs).trim().length;
-	return { commasCount, argsLength, endArgs };
+	return { commasCount, argsLength, endArgs, excessArgs };
 }
